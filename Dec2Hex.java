a00264923b719a0b475875b7a82557ddf3a1e105
JamesP98 <james.plimbley@googlemail.com>
2019-11-06 12:32:34 +0000

    Change made to java project to fix the break used for testing and in check for null values

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 class Dec2Hex
 {
-
     public static int Arg1;
 
     public static void main(String args[])
@@ -12,14 +11,25 @@ class Dec2Hex
             try 
             {
                 Arg1 = Integer.parseInt(args[0]);
-            } sadasd asdasd a
+            } 
             catch (NumberFormatException e) 
             {
                 System.err.println("Argument" + args[0] + " must be an integer.");
                 System.exit(1);
             }
-        }as dad as 
-a sad s das 
+        }
+		
+		if(args.length = null)
+		{
+		  try
+		  {
+			  Arg1 = Integer.parseInt(args[0])
+		  }
+		  catch (NumberFormatException e)
+		  {
+			  System.err.println("Argument" + args[0] + " must not be null."):
+		  }
+		}
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
          int rem, num;
 	num = Arg1;
